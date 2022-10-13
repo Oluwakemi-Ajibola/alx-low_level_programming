123b1f5818ee7c9707f6a952ab5daf34a4ed513c
Öluwakemi <kemiajibola.victoria@gmail.com>
2022-10-13 13:51:48 -0700

    4-puts.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints "Programming is like building a multilingual puzzle"
+ * main - Entry
  * Return : Always 0
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle",\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
