407615c81abf72988a66d47d5d3f36ee153ebc53
Öluwakemi <kemiajibola.victoria@gmail.com>
2022-10-14 15:21:36 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,16 +3,15 @@
 #include <time.h>
 /**
  * main - ENtry point
- * Return: ALways 0
+ * Return: Always 0
  */
-
 int main(void)
 {
 	char low;
 
 	for (low = 'z'; low >= 'a'; low--)
-		putchar(low)
+		putchar(low);
 		putchar('\n');
-
+	
 	return (0);
 }
