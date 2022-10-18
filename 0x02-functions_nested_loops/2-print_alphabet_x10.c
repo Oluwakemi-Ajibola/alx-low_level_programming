aec29321f9c79f95a3ccfdcbc9c6bd966b2d52ec
Öluwakemi <kemiajibola.victoria@gmail.com>
2022-10-18 08:34:46 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ * Return: Always 0
+ */
+void print_alphabet_x10(void);
+{
+	char i, b;
+	for (b = 0; b <= 9; b++)
+	{
+	for (i = 'a'; i <= 'z'; i++);
+	{
+	_putchar(i);
+	}
+_putchar('\n');
+}
+}
