ec952141a54876902f95bb62e2f5414e5bc4b3a1
Öluwakemi <kemiajibola.victoria@gmail.com>
2022-10-18 08:40:00 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,9 @@ void print_alphabet_x10(void)
 	char i, b;
 	for (b = 0; b <= 9; b++)
 	{
-	for (i = 'a'; i <= 'z'; i++);
-	{
-	_putchar(i);
+		for (i = 'a'; i <= 'z'; i++);
+		{
+		_putchar(i);
 	}
 _putchar('\n');
 }
