f19fef5241e18c2be3f253d8176addfa2ee56bbc
Öluwakemi <kemiajibola.victoria@gmail.com>
2022-10-20 07:01:58 -0700

    my nested loops commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0123456789 followed by a new line.
+ * print_numbers - print 0123456789
  * Return: void
  */
+
 void print_numbers(void)
 {
-	int i;
+	char c;
 
-	for (i = 0; i < 10; i++)
-		_putchar(i + '0';
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
 	_putchar('\n');
 }
