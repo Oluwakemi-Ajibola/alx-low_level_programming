2de8feb718678e6ab9c2cf4988b606209d77f684
Öluwakemi <kemiajibola.victoria@gmail.com>
2022-10-31 15:27:08 -0700

    Crachme

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,3 +8,5 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 int _putchar(char);
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
